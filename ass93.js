96fdbaff7c51d9312554a1002d1115527cf39b3f
Badavath saaryu Naik <2303a510a6@sru.edu.in>
2026-02-02 10:50:02 +0530

    Add files via upload

---
+++ ass93.js
@@ -0,0 +1,10 @@
+// Arthimatic operations
+let a = 10, b = 5;
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+// file creation and write
+const fs = require('fs');
+fs.writeFileSync("sample.txt", "This is a Node.js file");
+console.log("File created successfully");
